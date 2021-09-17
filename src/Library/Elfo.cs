559ac16f7957b3738b839a9dc7c28a2e3dbe278c
SantiRDL <santiago.rodriguezdl@correo.ucu.edu.uy>
2021-09-17 16:28:08 -0300

    Create Elfo.cs

---
+++ src/Library/Elfo.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Program
+{
+    class Elfo
+    {
+        public Estadisticas estadisticaselfo
+        
+        static void Main(string[] args)
+        {   
+        }
+    }
+}
