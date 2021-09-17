9b84d75fd68c212b15f466852c3138b70bb9cfe6
SantiRDL <santiago.rodriguezdl@correo.ucu.edu.uy>
2021-09-17 16:43:33 -0300

    Clase Elfo

--- src/Library/Elfo.cs
+++ src/Library/Elfo.cs
@@ -1,13 +1,64 @@
 using System;
 
-namespace Program
+namespace LibraryClass
 {
-    class Elfo
+    class Elfo 
     {
-        public Estadisticas estadisticaselfo
+        public string Nombre;
+        public Estadistica Estadistica = new Estadistica;
+        this.Estadistica.Ataque = 50;
+        this.Estadistica.Magia = 50;
+        this.Estadistica.Defensa = 50;
+        this.Estadistica.DefensaMagica =50;
+        this.Estadistica.PuntosDeVida = 90;
         
-        static void Main(string[] args)
-        {   
+        public Equipamineto Objeto = null;
+        public Elfos (string nombre)
+        {
+            this.Nombre = nombre;
+        }
+
+        public void AtaqueFisico (Enano personajeAtacado))
+        {
+            personajeAtacado.Estadistica.PuntosDeVida = personajeAtacado.Estadisticas.PuntosDeVida - this.Estadistica.Ataque;
+        }
+
+        public void AtaqueFisico (Gigante personajeAtacado)
+        {
+            personajeAtacado.Estadistica.PuntosDeVida = personajeAtacado.Estadisticas.PuntosDeVida - this.Estadistica.Ataque;
+        }
+
+        public void AtaqueFisico (Mago personajeAtacado)
+        {
+            personajeAtacado.Estadistica.PuntosDeVida = personajeAtacado.Estadisticas.PuntosDeVida - this.Estadistica.Ataque;
+        }
+
+        public void EquiparObjeto (Equipamiento objeto)
+        {
+            this.Objeto = objeto;
+            this.Estadistica.Ataque += objeto.Estadistica.Ataque;
+            this.Estadistica.Magia += objeto.Estadistica.Magia;
+            this.Estadistica.Defensa += objeto.Estadistica.Defensa;
+            this.Estadistica.DefensaMagica += objeto.Estadistica.DefensaMagica;
+            this.Estadistica.PuntosDeVida += objeto.Estadistica.PuntosDeVida;       
+        }
+
+        public void DesequiparObjeto (Equipamiento objeto)
+        {
+            if (this.Objeto != null)
+            {
+                this.Objeto = null;
+                this.Estadistica.Ataque -= objeto.Estadistica.Ataque;
+                this.Estadistica.Magia -= objeto.Estadistica.Magia;
+                this.Estadistica.Defensa -= objeto.Estadistica.Defensa;
+                this.Estadistica.DefensaMagica -= objeto.Estadistica.DefensaMagica;
+                this.Estadistica.PuntosDeVida -= objeto.Estadistica.PuntosDeVida;
+            }
+
+            else
+            {
+                Console.WriteLine ("No hay objetos para desequipar")
+            }
         }
     }
 }
