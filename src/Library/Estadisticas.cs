befb7fe86bc55d6062048de1f7460bf02b62c1e4
SantiRDL <santiago.rodriguezdl@correo.ucu.edu.uy>
2021-09-17 16:44:48 -0300

    Modificacion mayusculas en variable puntos de vida

--- src/Library/Estadisticas.cs
+++ src/Library/Estadisticas.cs
@@ -6,7 +6,7 @@ namespace Program
     {
         public Estadisticas(int PuntosDeVida, int Ataque, int Magia, int Defensa, int DefensaMagica)
         {
-            this.PuntosDeVida = PuntosDeVida
+            this.PuntosDeVida = puntosdevida
             this.Ataque = ataque
             this.Magia = magia
             this.Defensa = Defensa
@@ -18,6 +18,6 @@ namespace Program
         public int Magia { get; set; }
         public int Defensa { get; set; }
         public int DefensaMagica { get; set; }
-        
+
     }
 }
