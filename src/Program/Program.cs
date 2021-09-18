afeb451fa50658ff99a9a0a989e7c006997d67b8
Ionasjospo <88661713+Ionasjospo@users.noreply.github.com>
2021-09-17 22:39:36 -0300

    Creación objetos

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -10,6 +10,10 @@ namespace Program
             Equipamiento espada = new Equipamiento("espada", "arma", 0, 10, 0, 0 ,0);
             Equipamiento capa = new Equipamiento("capa", "ropaje", 0, 0, 0, 10 ,10);
 
+            Equipamiento arco = new Equipamiento("Arco", "arma", 0, 30, 0, 0, 0 );
+            Equipamiento martillo = new Equipamiento("Martillo", "arma", 0, 25, 0, 0, 0 );
+
+
         }
     }
 }
