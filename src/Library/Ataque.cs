1e2f017247454f00b1477c253a8b7bb1528fa623
Ionasjospo <88661713+Ionasjospo@users.noreply.github.com>
2021-09-17 22:18:36 -0300

    Comentario

--- src/Library/Ataque.cs
+++ src/Library/Ataque.cs
@@ -2,8 +2,14 @@ using System;
 
 namespace LibraryClass
 {
+    
     class Ataque
     {
+        /// <summary>
+        /// Este método hace que se le quiten los puntos de vida del personaje2, con los puntos de ataque del personaje1. 
+        /// </summary>
+        /// <param name="personaje1"></param>
+        /// <param name="personaje2"></param>
         public static void AtaqueFisico(Elfo personaje1, Enano personaje2)
         {
             personaje2.Estadisticas.PuntosDeVida -= personaje1.Estadisticas.Ataque;
